8e863c9526cbe66c9ea5e956aa2d1df36964b6c6
Farnood Massoudi <fa.massoudi@gmail.com>
2022-10-31 02:57:52 -0400

    add basic errors

---
+++ src/errors.ts
@@ -0,0 +1,9 @@
+import { RC_LOCATION } from './constants'
+
+export const SERVER_ERROR =
+  'Something went wrong with the server. Please try again later.'
+export const NOT_LOGGED_IN = 'You need to login first by running `bettermode login`.'
+export const INVALID_TOKEN =
+  'Your token is invalid or has been expired. Please login again.'
+export const NO_ACCESS_TO_CONFIG_ERROR = `You do not have access to the config file in \`${RC_LOCATION}\`.`
+export const INVALID_EMAIL = 'Please enter a valid email address.'
