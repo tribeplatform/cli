3c55d4ebe40e75d5352aa5c13c1fcaa554b9d5e8
Farnood Massoudi <fa.massoudi@gmail.com>
2022-11-07 22:13:26 -0500

    create both dev and prod apps with dev option

--- src/commands/app/create.ts
+++ src/commands/app/create.ts
@@ -1,11 +1,11 @@
-import { App } from '@tribeplatform/gql-client/global-types'
+import { App, Network } from '@tribeplatform/gql-client/global-types'
 import { BetterCommand } from '../../better-command'
 import {
   getCreateAppInputs,
   getCreateAppTasks,
   removeCreateAppTargetDirs,
 } from '../../logics'
-import { getBettermodeTypeface, UnAuthorizedError } from '../../utils'
+import { CliClient, getBettermodeTypeface, UnAuthorizedError } from '../../utils'
 
 type CreateAppResponse = App
 
@@ -15,31 +15,42 @@ export default class CreateApp extends BetterCommand<CreateAppResponse> {
   static examples = [`$ bettermode app create`]
 
   async run(): Promise<CreateAppResponse> {
+    const { dev } = await this.getGlobalFlags()
+    const { officialPartner } = await this.getGlobalConfigs()
+
+    const client = await this.getClient(false)
+    const networks = await this.getNetworks(false)
+
+    let devClient: CliClient | null = null
+    let devNetworks: Network[] = []
+    if (dev) {
+      devClient = await this.getClient(true)
+      devNetworks = await this.getNetworks(true)
+    }
+
+    const githubUser = await this.getGithubUser()
+
+    if (!client || (dev && !devClient)) {
+      this.log(`${!client} - ${!devClient}`)
+      throw new UnAuthorizedError()
+    }
+
+    if (networks.length === 0 || (dev && devNetworks.length === 0)) {
+      throw new UnAuthorizedError(`You don't have any networks, please create one first.`)
+    }
+
     const typeface = await getBettermodeTypeface()
     this.log(typeface || '')
     this.log(`Let's create your next amazing app!\n\n`)
 
-    const { dev } = await this.getGlobalFlags()
-    const { officialPartner } = await this.getGlobalConfigs()
-    const client = await this.getClient()
-    const networks = await this.getNetworks()
-    const githubUser = await this.getGithubUser()
-
-    if (!client) {
-      throw new UnAuthorizedError()
-    }
-
-    if (networks.length === 0) {
-      throw new UnAuthorizedError(`You don't have any networks, please create one first.`)
-    }
-
     const input = await this.prompt(
-      getCreateAppInputs({ networks, officialPartner, githubUser }),
+      getCreateAppInputs({ networks, devNetworks, officialPartner, githubUser }),
     )
 
     const tasks = getCreateAppTasks({
       dev,
       client,
+      devClient,
       officialPartner,
       input,
     })
--- src/logics/create-app.logics.ts
+++ src/logics/create-app.logics.ts
@@ -2,6 +2,7 @@ import { Prompter } from '@salesforce/sf-plugins-core'
 import {
   App,
   AppCollaborator,
+  CreateAppInput,
   DefaultDynamicBlockKeys,
   DynamicBlock,
   Network,
@@ -16,12 +17,14 @@ import { CliClient, CliError, Shell } from '../utils'
 import { getSyncAppTasks } from './sync-app.logics'
 
 export type CreateAppCLIInputs = {
+  devNetworkId?: string
   networkId: string
   name: string
   slug?: string
   standing?: StoreItemStanding
   description: string
   domain: string
+  devDomain?: string
   repoOwner: string
   repoName: string
   authorName: string
@@ -32,12 +35,24 @@ export type CreateAppCLIInputs = {
 }
 
 export const getCreateAppInputs = (options: {
+  devNetworks: Network[]
   networks: Network[]
   githubUser: GithubUser | null
   officialPartner?: boolean
 }): Prompter.Questions<CreateAppCLIInputs> => {
-  const { networks, githubUser, officialPartner = false } = options
+  const { devNetworks, networks, githubUser, officialPartner = false } = options
   const result: Prompter.Questions<CreateAppCLIInputs> = [
+    {
+      name: 'devNetworkId',
+      type: 'list',
+      default: devNetworks[0].id,
+      message: `App's network in dev environment`,
+      choices: devNetworks.map(network => ({
+        name: network.domain,
+        value: network.id,
+      })),
+      when: devNetworks.length > 0,
+    },
     {
       name: 'networkId',
       type: 'list',
@@ -79,6 +94,14 @@ export const getCreateAppInputs = (options: {
         value: standing,
       })),
       when: officialPartner,
+    },
+    {
+      name: 'devDomain',
+      type: 'input',
+      message: `App's domain in dev environment`,
+      default: ({ slug }: { slug: string }) => `${slug}-dev.tribeplatform.app`,
+      validate: (domain: string) =>
+        /^(?:[\dA-Za-z]+(?:-[\dA-Za-z]+)*\.){2}[\dA-Za-z]+$/.test(domain),
     },
     {
       name: 'domain',
@@ -173,37 +196,91 @@ export const removeCreateAppTargetDirs = (input: CreateAppCLIInputs) => {
   Shell.rm(join(process.cwd(), targetDir), { silent: true })
 }
 
+export const createApp = async (options: {
+  client: CliClient
+  domain: string
+  officialPartner: boolean
+  withDynamicSettings: boolean
+  input: CreateAppInput
+}): Promise<App> => {
+  const { client, domain, officialPartner, withDynamicSettings, input } = options
+  const { name, networkId, slug, description, authorName, authorUrl, standing } = input
+  const app = await client.mutation({
+    name: 'createApp',
+    args: {
+      variables: {
+        input: {
+          name,
+          networkId,
+          slug,
+          description,
+          authorName,
+          authorUrl,
+          standing,
+          webhookUrl: `https://${domain}/webhook`,
+          interactionUrl: `https://${domain}/interaction`,
+          federatedSearchUrl: `https://${domain}/federated-search`,
+          privacyPolicyUrl: officialPartner
+            ? `https://bettermode.io/privacy-policy`
+            : null,
+          termsOfServiceUrl: officialPartner
+            ? `https://bettermode.io/terms-of-service`
+            : null,
+        },
+      },
+      fields: {
+        customCodes: 'all',
+        favicon: 'all',
+        image: 'all',
+      },
+    },
+  })
+  if (withDynamicSettings) {
+    await client.mutation({
+      name: 'enableDefaultDynamicBlock',
+      args: {
+        variables: {
+          appId: app.id,
+          key: DefaultDynamicBlockKeys.settings,
+          input: {},
+        },
+        fields: 'basic',
+      },
+    })
+  }
+
+  return app
+}
+
 export const getCreateAppTasks = (options: {
   dev: boolean
   client: CliClient
+  devClient: CliClient | null
   officialPartner?: boolean
   input: CreateAppCLIInputs
 }): Listr<{
   app: App
+  devApp?: App
   collaborators: AppCollaborator[]
+  devCollaborators?: AppCollaborator[]
   shortcuts: Shortcut[]
+  devShortcuts?: Shortcut[]
   blocks: DynamicBlock[]
+  devBlocks?: DynamicBlock[]
 }> => {
+  const { dev, client, devClient, officialPartner = false, input } = options
   const {
-    dev,
-    client,
-    officialPartner = false,
-    input: {
-      networkId,
-      name,
-      slug,
-      standing,
-      description,
-      domain,
-      template,
-      withDynamicSettings,
-      repoOwner,
-      repoName,
-      authorName,
-      authorUrl,
-      license,
-    },
-  } = options
+    devNetworkId,
+    description,
+    devDomain,
+    domain,
+    template,
+    withDynamicSettings,
+    repoOwner,
+    repoName,
+    authorName,
+    license,
+  } = input
 
   const { targetDir, tmpDir } = getCreateAppTargetDirs(repoName)
   const cwd = join(process.cwd(), targetDir)
@@ -233,51 +310,50 @@ export const getCreateAppTasks = (options: {
     },
     {
       title: 'Create the app in the portal',
+      enabled: () => !dev,
       task: async ctx => {
-        ctx.app = await client.mutation({
-          name: 'createApp',
-          args: {
-            variables: {
-              input: {
-                name,
-                networkId,
-                slug,
-                description,
-                authorName,
-                authorUrl,
-                standing,
-                webhookUrl: `https://${domain}/webhook`,
-                interactionUrl: `https://${domain}/interaction`,
-                federatedSearchUrl: `https://${domain}/federated-search`,
-                privacyPolicyUrl: officialPartner
-                  ? `https://bettermode.io/privacy-policy`
-                  : null,
-                termsOfServiceUrl: officialPartner
-                  ? `https://bettermode.io/terms-of-service`
-                  : null,
-              },
-            },
-            fields: {
-              customCodes: 'all',
-              favicon: 'all',
-              image: 'all',
+        ctx.app = await createApp({
+          client,
+          domain,
+          officialPartner,
+          withDynamicSettings,
+          input,
+        })
+      },
+    },
+    {
+      title: 'Create the app in the portal',
+      enabled: () => dev,
+      task: () =>
+        new Listr([
+          {
+            title: 'on production environment',
+            task: async ctx => {
+              ctx.app = await createApp({
+                client,
+                domain,
+                officialPartner,
+                withDynamicSettings,
+                input,
+              })
             },
           },
-        })
-        if (withDynamicSettings) {
-          await client.mutation({
-            name: 'enableDefaultDynamicBlock',
-            args: {
-              variables: {
-                appId: ctx.app.id,
-                key: DefaultDynamicBlockKeys.settings,
-                input: {},
-              },
-              fields: 'basic',
+          {
+            title: 'on development environment',
+            task: async ctx => {
+              ctx.devApp = await createApp({
+                client: devClient as CliClient,
+                domain: devDomain as string,
+                officialPartner,
+                withDynamicSettings,
+                input: {
+                  ...input,
+                  networkId: devNetworkId as string,
+                },
+              })
             },
-          })
-        }
-      },
+          },
+        ]),
     },
     {
       title: 'Setup the app',
@@ -291,11 +367,30 @@ export const getCreateAppTasks = (options: {
             {
               title: 'Adding app secrets to env files',
               task: async () => {
-                const app = ctx.app as App
+                const app = ctx.app
+                const devApp = ctx.devApp
 
                 Shell.cp('.env.example', '.env.development.local', { cwd })
                 Shell.cp('.env.example', '.env.production.local', { cwd })
                 Shell.cp('.env.example', '.env.test.local', { cwd })
+                Shell.replaceString(
+                  [
+                    {
+                      search: 'client-id',
+                      replacement: (devApp || app)?.clientId as string,
+                    },
+                    {
+                      search: 'client-secret',
+                      replacement: (devApp || app)?.clientSecret as string,
+                    },
+                    {
+                      search: 'signing-secret',
+                      replacement: (devApp || app)?.webhookSignSecret as string,
+                    },
+                  ],
+                  ['.env.development.local'],
+                  { cwd },
+                )
                 Shell.replaceString(
                   [
                     { search: 'client-id', replacement: app?.clientId as string },
@@ -308,11 +403,6 @@ export const getCreateAppTasks = (options: {
                       replacement: app?.webhookSignSecret as string,
                     },
                   ],
-                  ['.env.development.local', '.env.production.local'],
-                  { cwd },
-                )
-                Shell.replaceString(
-                  { search: 'app-domain', replacement: domain },
                   ['.env.production.local'],
                   { cwd },
                 )
@@ -327,10 +417,15 @@ export const getCreateAppTasks = (options: {
                     { search: 'app-description', replacement: description },
                     { search: 'app-author', replacement: authorName },
                     { search: 'app-license', replacement: license },
+                    {
+                      search: 'app-year',
+                      replacement: new Date().getFullYear().toString(),
+                    },
                   ],
                   [
                     'Makefile',
                     'package.json',
+                    'LICENSE.md',
                     join('.circleci', 'config.yml'),
                     join('.vscode', 'launch.json'),
                   ],
@@ -349,7 +444,18 @@ export const getCreateAppTasks = (options: {
     },
     {
       title: `Initialize app's config`,
-      task: ctx => getSyncAppTasks({ client, app: ctx.app as App, dev, cwd }),
+      task: ctx => getSyncAppTasks({ client, app: ctx.app, dev: false, cwd }),
+    },
+    {
+      title: `Initialize app's config for development`,
+      enabled: () => dev,
+      task: ctx =>
+        getSyncAppTasks({
+          client: devClient as CliClient,
+          app: ctx.devApp as App,
+          dev: true,
+          cwd,
+        }),
     },
     {
       title: 'Setup git repository',
@@ -363,8 +469,8 @@ export const getCreateAppTasks = (options: {
           await Shell.exec('git add --all', { cwd })
           await Shell.exec('git commit -m "Initial setup"', { cwd })
           await Shell.exec('git push -u origin main', { cwd })
-        } catch {
-          task.skip('Git repository not initialized, please do it manually.')
+        } catch (error) {
+          task.skip((error as Error)?.toString() || 'Failed to setup git repository')
         }
       },
     },
