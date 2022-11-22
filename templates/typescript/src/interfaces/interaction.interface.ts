6cb66f17796df354b233c5fc92bfccaca0aa0bd6
Farnood Massoudi <fa.massoudi@gmail.com>
2022-11-21 21:37:40 -0500

    add interaction props interfaces

--- templates/typescript/src/interfaces/interaction.interface.ts
+++ templates/typescript/src/interfaces/interaction.interface.ts
@@ -1,5 +1,30 @@
 import { InteractionType } from '@enums'
+import { ToastStatus } from '@prisma/client'
+import { PermissionContext } from '@tribeplatform/gql-client/types'
 import { SlateDto } from '@tribeplatform/slate-kit/dtos'
+
+export type ContainerSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'full'
+
+export interface ToastInteractionProps {
+  status: ToastStatus
+  title: string
+  description?: string
+}
+
+export interface ModalInteractionProps {
+  size: ContainerSize
+  title: string
+  description?: string
+}
+
+export interface ReloadInteractionProps {
+  entity: PermissionContext
+}
+
+export interface RedirectInteractionProps {
+  url: string
+  external?: boolean
+}
 
 export interface InteractionInput<T = { [key: string]: unknown }> {
   actorId: string
@@ -24,22 +49,23 @@ export interface CloseInteraction extends BaseInteraction {
 
 export interface OpenModalInteraction extends BaseInteraction {
   type: InteractionType.OpenModal
-  props: { [key: string]: unknown }
+  props: ModalInteractionProps
   slate: SlateDto
 }
 
 export interface OpenToastInteraction extends BaseInteraction {
   type: InteractionType.OpenToast
-  props: { [key: string]: unknown }
+  props: ToastInteractionProps
 }
 
 export interface RedirectInteraction extends BaseInteraction {
   type: InteractionType.Redirect
-  props: { [key: string]: unknown }
+  props: RedirectInteractionProps
 }
 
 export interface ReloadInteraction extends BaseInteraction {
   type: InteractionType.Reload
+  props: ReloadInteractionProps
 }
 
 export interface ShowInteraction extends BaseInteraction {
